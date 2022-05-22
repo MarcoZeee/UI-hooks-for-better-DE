c496838607864eca76d611f2685e72dc42e2c5a6
MarcoZeee <MarcoZeee@github.com>
2022-05-22 09:57:21 +0800

    feat: progressBar upgrade

--- src/hooks/useScrollBar.jsx
+++ src/hooks/useScrollBar.jsx
@@ -1,29 +1,42 @@
-import { useState, useRef, useEffect } from "react";
-import {createPortal } from "react-dom";
-
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 
 const useScrollBar = () => {
-    const bar = document.querySelector("#bar");
+  const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(false);
+  const [barSize, setBarSize] = useState(0);
+  let max = document.body.scrollHeight - window.innerHeight;
+
+  useEffect(() => {
     const handleScroll = () => {
-        let max = document.body.scrollHeight - window.innerHeight;
-        bar.style.width = `${window.scrollY / max * 100}%`;
-    }
-    window.addEventListener("scroll", handleScroll);
-    return createPortal(
-    <>
-        <div id="#bar" style={
-            {
-                position: "fixed",
-                top: 0,
-                left: 0,
-                width: 0,
-                'border-bottom': "2px solid #ccc",
-            }
-        }>
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current < currentScrollY && goingUp) {
+        setGoingUp(false);
+      }
+      if (prevScrollY.current > currentScrollY && !goingUp) {
+        setGoingUp(true);
+      }
 
-        </div>
-    </>
-, document.body);
-    }
+      prevScrollY.current = currentScrollY;
+      setBarSize((currentScrollY / max) * 100 + "%");
+      console.log(goingUp, currentScrollY, barSize);
+    };
 
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [barSize]);
+
+  const Bar = styled.div`
+    border-bottom: 5px solid green;
+    width: ${(props) => props.barSize};
+    position: fixed;
+    top: 0;
+    left: 0;
+  `;
+  return {
+    barSize,
+    Bar
+  };
+};
 export default useScrollBar;
